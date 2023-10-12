8b2a44f346da0d0ff061a700799a76dff0716246
rex huang <Rexh09@gmail.com>
2023-10-11 20:40:14 -0700

    ALMOST WORKING LOCALSTORAGE ON v2 :partyparrot:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -175,7 +175,7 @@ function handlefinish(task: string) {
 </div>
 
 
-
+ 
 
 
 
--- src/routes/v2/+page.svelte
+++ src/routes/v2/+page.svelte
@@ -2,114 +2,34 @@
     import type { Todo } from "$lib/types";
 
     let tasks: Todo[] = [];
+    let count = 0
     let color = "white";
     let color2 = "black";
     let colors = ["red", "orange", "yellow", "green", "blue"];
     let finished: string[] = [];
-    let today =
-        new Date().getFullYear() +
-        "-" +
-        ("0" + (new Date().getMonth() + 1)).slice(-2) +
-        "-" +
-        ("0" + new Date().getDate()).slice(-2);
     let input = "";
     let randomized: string[] = [];
     let funnyrandom = "https://cultofthepartyparrot.com/parrots/parrot.gif";
-    let parrots: string[] = [
-        "bobrossparrot.gif",
-        "portalparrot.gif",
-        "pythonparrot.gif",
-        "revertitparrot.gif",
-        "rotatingparrot.gif",
-        "ryangoslingparrot.gif",
-        "schnitzelparrot.gif",
-        "shipitparrot.gif",
-        "shufflefurtherparrot.gif",
-        "shufflepartyparrot.gif",
-        "skiparrot.gif",
-        "slomoparrot.gif",
-        "sovjetparrot.gif",
-        "stalkerparrot.gif",
-        "standupparrot.gif",
-        "tacoparrot.gif",
-        "tripletsparrot.gif",
-        "uparrowparrot.gif",
-        "upvoteparrot.gif",
-        "wave1parrot.gif",
-        "wave2parrot.gif",
-        "wave3parrot.gif",
-        "wave4parrot.gif",
-        "wave5parrot.gif",
-        "wave6parrot.gif",
-        "wave7parrot.gif",
-        "wave8parrot.gif",
-        "wave9parrot.gif",
-        "chillparrot.gif",
-        "coffeeparrot.gif",
-        "cryptoparrot.gif",
-        "dabparrot.gif",
-        "darkbeerparrot.gif",
-        "databaseparrot.gif",
-        "deployparrot.gif",
-        "dizzyparrot.gif",
-        "docparrot.gif",
-        "dreidelparrot.gif",
-        "drupal_parrot.gif",
-        "ethparrot.gif",
-        "explodyparrot.gif",
-        "fadingparrot.gif",
-        "fidgetparrot.gif",
-        "fiestaparrot.gif",
-        "fixparrot.gif",
-        "halalparrot.gif",
-        "hamburgerparrot.gif",
-        "harrypotterparrot.gif",
-        "icecreamparrot.gif",
-        "krakenparrot.gif",
-        "loveparrot.gif",
-        "luckyparrot.gif",
-        "margaritaparrot.gif",
-        "matrixparrot.gif",
-        "mergedparrot.gif",
-        "metalparrot.gif",
-        "mongodbparrot.gif",
-        "oldtimeyparrot.gif",
-        "papalparrot.gif",
-        "parrot.gif",
-        "pear-parrots.gif",
-        "pizzaparrot.gif",
-        "asyncparrot.gif",
-        "bananaparrot.gif",
-        "blondesassyparrot.gif",
-        "bluescluesparrot.gif",
-    ];
-    let randomElement ="https://cultofthepartyparrot.com/parrots/" + parrots[Math.floor(Math.random() * parrots.length)];
+    let parrots: string[] = ["bobrossparrot.gif", "portalparrot.gif", "pythonparrot.gif", "revertitparrot.gif", "rotatingparrot.gif", "ryangoslingparrot.gif", "schnitzelparrot.gif", "shipitparrot.gif", "shufflefurtherparrot.gif", "shufflepartyparrot.gif", "skiparrot.gif", "slomoparrot.gif", "sovjetparrot.gif", "stalkerparrot.gif", "standupparrot.gif", "tacoparrot.gif", "tripletsparrot.gif", "uparrowparrot.gif", "upvoteparrot.gif", "wave1parrot.gif", "wave2parrot.gif", "wave3parrot.gif", "wave4parrot.gif", "wave5parrot.gif", "wave6parrot.gif", "wave7parrot.gif", "wave8parrot.gif", "wave9parrot.gif", "chillparrot.gif", "coffeeparrot.gif", "cryptoparrot.gif", "dabparrot.gif", "darkbeerparrot.gif", "databaseparrot.gif", "deployparrot.gif", "dizzyparrot.gif", "docparrot.gif", "dreidelparrot.gif", "drupal_parrot.gif", "ethparrot.gif", "explodyparrot.gif", "fadingparrot.gif", "fidgetparrot.gif", "fiestaparrot.gif", "fixparrot.gif", "halalparrot.gif", "hamburgerparrot.gif", "harrypotterparrot.gif", "icecreamparrot.gif", "krakenparrot.gif", "loveparrot.gif", "luckyparrot.gif", "margaritaparrot.gif", "matrixparrot.gif", "mergedparrot.gif", "metalparrot.gif", "mongodbparrot.gif", "oldtimeyparrot.gif", "papalparrot.gif", "parrot.gif", "pear-parrots.gif", "pizzaparrot.gif", "asyncparrot.gif", "bananaparrot.gif", "blondesassyparrot.gif", "bluescluesparrot.gif",];
+    function resetclick(){
+        tasks.length = 0
+        count == 0
+    }
     function funnyfunction() {
         funnyrandom ="https://cultofthepartyparrot.com/parrots/" + parrots[Math.floor(Math.random() * parrots.length)];
         color = colors[Math.floor(Math.random() * colors.length)];
         color2 = colors[Math.floor(Math.random() * colors.length)];
+        while (color == color2){
+        color = colors[Math.floor(Math.random() * colors.length)];
+        color2 = colors[Math.floor(Math.random() * colors.length)];}
         randomized.forEach((element) => {});
     }
     function handlefinish(taskToComplete: string) {
-        console.log(taskToComplete);
-
+        count = count+1
         tasks.forEach((task) => {
             if ((task.task == taskToComplete)) {
-                task.done = true;
-            }
-        });
-        tasks = tasks
-    }
-
-    // let idx = tasks.indexOf(task);
-    // 		console.log(task)
-    // 		finished.push(tasks[idx])
-    // 		tasks.splice(idx, 1);
-    // 		randomElement = "https://cultofthepartyparrot.com/parrots/"+parrots[Math.floor(Math.random() * parrots.length)];
-    // 		randomized.push(randomElement)
-    // 		finished = finished
-    // 		tasks = tasks
-    // 		randomized = randomized
+                task.done = true;}});
+        tasks = tasks}
 
     function handleclick() {
         if (input !== "") {
@@ -122,48 +42,21 @@
                     "-" +
                     ("0" + (new Date().getMonth() + 1)).slice(-2) +
                     "-" +
-                    ("0" + new Date().getDate()).slice(-2),
-            });
+                    ("0" + new Date().getDate()).slice(-2),});
             tasks = tasks;
-            input = ""
-        }
-    }
+            input = ""}}
 
-    // if (finished.length == 0){
-    // 	finished = JSON.parse(localStorage.getItem("finished") || "");}
-
-    // if (dates.length == 0){
-    // 	dates = JSON.parse(localStorage.getItem("dates") || "");}
-    // if (tasks.length == 1){
-    // tasks = JSON.parse(localStorage.getItem("tasks")||"");}
-
-    // if (randomized.length == 0){
-    // randomized = JSON.parse(localStorage.getItem("randomized")||"");
-    // }
-    //  {
-    // localStorage.setItem("randomized", JSON.stringify(randomized));
-    // localStorage.setItem("finished", JSON.stringify(finished));
-    // localStorage.setItem("tasks", JSON.stringify(tasks));
-    // localStorage.setItem("dates", JSON.stringify(dates));
-
-    // }
+   
+if (tasks.length == 0){
+tasks = JSON.parse(localStorage.getItem("tasks")||"");}
+$: localStorage.setItem("tasks", JSON.stringify(tasks));
+// count = localStorage.getItem("count")||"";
+// localStorage.setItem("count", (count));
 </script>
 
 <title>Unimportant Title Lmao</title>
-
 <div class="site">
-    <h1>
-        <img
-            style="height: 40px;"
-            src="https://cultofthepartyparrot.com/parrots/parrot.gif"
-            alt="partyparrot"
-        />Rex's Scuffed To Do List!<img
-            style="height: 40px;"
-            src="https://cultofthepartyparrot.com/parrots/parrot.gif"
-            alt="partyparrot"
-        />
-    </h1>
-    <br />
+    <h1><img style="height: 40px;" src="https://cultofthepartyparrot.com/parrots/parrot.gif" alt="partyparrot"/>Rex's Scuffed To Do List!<img style="height: 40px;" src="https://cultofthepartyparrot.com/parrots/parrot.gif" alt="partyparrot"/></h1><br/>
     <p>Input New Tasks One At A Time:</p>
     <input
         type="text-box"
@@ -175,47 +68,26 @@
 
     {#each tasks as task}
         {#if task.done == false}
-        <button class="task" on:click={() => handlefinish(task.task)}
+        <button style="background-color: {color2}; color: {color}" class="task" on:click={() => handlefinish(task.task)}
             >{task.task}</button><br>
         {/if}
-        {#if task.done == true}
-        <p><i>you have no tasks lol which either means your the most productive person in the world, or so lazy as to not even manage your todo list</i></p>
-    {/if}
     {/each}
-
+    {#if count == tasks.length}
+    <p><i>you have no tasks lol which either means your the most productive person in the world, or so lazy as to not even manage your todo list</i></p>
+    {/if}
     <h1><img style="height: 40px;" src="https://cultofthepartyparrot.com/parrots/parrot.gif" alt="partyparrot">Completed Tasks!<img style="height: 40px;" src="https://cultofthepartyparrot.com/parrots/parrot.gif" alt="partyparrot"></h1>
     <br>
     {#each tasks as task}
         {#if task.done == true}
-        <h4 style="font-size: 50px;">"{task.task}" is done! <img src={`https://cultofthepartyparrot.com/parrots/${task.parrotpath}`} alt = "partyparrot"/></h4><br>        
+        <h4 style="font-size: 50px;">"{task.task}" is done! <img src={`https://cultofthepartyparrot.com/parrots/${task.parrotpath}`} alt = "partyparrot"/></h4>
+        <p>Started on {task.date}</p>
         {/if}
-        {:else}
-        <p>get to work idiot</p>
     {/each}
-    <!-- {#if (tasks.length) != 0}
-	{#each tasks as task}
-		{#if (color == color2)}
-		<button class="task" style="color: black; background-color: white" on:click={ () => handlefinish(task) }>{task}</button><br>
-		{:else}
-		<button class="task" style="color: {color2}; background-color: {color}" on:click={ () => handlefinish(task) }>{task}</button><br>
-		{/if}
-	{/each}
-	{:else}
-	<p><i>you have no tasks lol which either means your the most productive person in the world, or so lazy as to not even manage your todo list</i></p>
-	{/if}
-	<br><br><br><br>
-	<h1><img style="height: 40px;" src="https://cultofthepartyparrot.com/parrots/parrot.gif" alt="partyparrot">Completed Tasks!<img style="height: 40px;" src="https://cultofthepartyparrot.com/parrots/parrot.gif" alt="partyparrot"></h1>
-	{#if (finished.length) !=0}
-	{#each finished as finish,i}
-	<h4 style="font-size: 50px;">"{finish}" is done! <img src={randomized[i]} alt = "partyparrot"/></h4>
-	<p> Started on {dates[i]}</p>
-	{/each}
-	{:else}
-	<p>get to work idiot</p>
-	{/if}
-	<button on:click = {resetclick}>Reset All Tasks <img src="https://cultofthepartyparrot.com/parrots/parrot.gif" alt="partyparrot"></button> <br><br><br><br>
-
-	<button on:click = {funnyfunction}>Need to destress? Click here! <img src="{funnyrandom}" alt="woulda been a random partyparrot"></button> -->
+    {#if count == 0}
+        <p>get to work idiot</p>
+    {/if}
+    <button on:click = {resetclick}>Reset All Tasks <img src="https://cultofthepartyparrot.com/parrots/parrot.gif" alt="partyparrot"></button> <br><br><br><br>
+	<button on:click = {funnyfunction}>Need to destress? Click here! <img src="{funnyrandom}" alt="woulda been a random partyparrot"></button>
 </div>
 
 <style>
@@ -263,28 +135,8 @@
             background-position: 0% 50%;
         }
     }
-    @-moz-keyframes rainbowanim {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
-    }
-    @keyframes rainbowanim {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
-    }
+    @-moz-keyframes rainbowanim {0% {background-position: 0% 50%;}50% {background-position: 100% 50%;}100% {background-position: 0% 50%;}}
+    @keyframes rainbowanim {0% {background-position: 0% 50%;}50% {background-position: 100% 50%;}100% {background-position: 0% 50%;}}
     .site {
         margin: 25px;
         border-style: solid;
@@ -292,8 +144,5 @@
         border-color: red;
         border-radius: 10px;
         padding: 10px;
-
         color: darkmagenta;
-        background-color: lightblue;
-    }
-</style>
+        background-color: lightblue;}</style>
